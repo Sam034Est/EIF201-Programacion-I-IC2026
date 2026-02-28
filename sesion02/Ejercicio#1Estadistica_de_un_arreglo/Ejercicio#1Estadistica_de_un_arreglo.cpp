a23047751dee9c6c2c9c189fd85b9913ba400d7b
Samuel Zamora Q <samuel.zamora.quintanilla@est.una.ac.cr>
2026-02-28 14:37:22 -0600

    finalizacion de ejercicio #1

--- sesion02/Ejercicio#1Estadistica_de_un_arreglo/Ejercicio#1Estadistica_de_un_arreglo.cpp
+++ sesion02/Ejercicio#1Estadistica_de_un_arreglo/Ejercicio#1Estadistica_de_un_arreglo.cpp
@@ -1,35 +1,36 @@
 // Ejercicio#1Estadistica_de_un_arreglo.cpp : This file contains the 'main' function.
 #include <iostream>
-#include <array>
 
 void menu();
-double numHighest(double inNum[]);
+double numHighest(double numHighest[]);
 
 int main(){
 
 	menu();
 
-    system("pause");
+    
     return 0;
 }
 
 
 void menu(){	
-	
-	std::array<double, 10 > inNum = {};
+
+	double inNum[10];
 
 
 	std::cout << "Ingrese 10 Numeros: " << std::endl;
 	
 	for (size_t i = 0; i < 10; i++)
 	{
-		//int aux =+ 1;
 		std::cout << "Valor#" << i+1 << ": ";
 		std::cin >> inNum[i];
 	}
+	
+	std::cout << "\nEl numero mayor fue: " << numHighest(inNum) << std::endl;
 
-	std::cout << "\nEl numero mayor es: " << numHighest(inNum[]);
+	system("pause");
 
+	
 }
 
 double numHighest(double inNum[])
@@ -37,10 +38,10 @@ double numHighest(double inNum[])
 	double topNum = 0;
 	for (size_t i = 0; i < 10; i++)
 	{
-		do
+		if (inNum[i] > topNum)
 		{
 			topNum = inNum[i];
-		} while (inNum[i] < inNum[i+1]);
+		}
 	}
 
 	return topNum;
