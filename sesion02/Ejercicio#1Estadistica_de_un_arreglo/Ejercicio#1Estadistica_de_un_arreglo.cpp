c8bb13599d28cbc779af4304a6cc8dd1a8c01cf1
Sam034Est <samuel.zamora.quintanilla@est.una.ac.cr>
2026-03-04 09:41:15 -0600

    ejercicio #1

--- sesion02/Ejercicio#1Estadistica_de_un_arreglo/Ejercicio#1Estadistica_de_un_arreglo.cpp
+++ sesion02/Ejercicio#1Estadistica_de_un_arreglo/Ejercicio#1Estadistica_de_un_arreglo.cpp
@@ -2,7 +2,10 @@
 #include <iostream>
 
 void menu();
-double numHighest(double numHighest[]);
+double numHighest(double inNum[], int size);
+double numLower(double inNum[], int size);
+double suma(double inNum[], int size);
+double average(double inNum[], int size);
 
 int main(){
 
@@ -16,27 +19,32 @@ int main(){
 void menu(){	
 
 	double inNum[10];
+	const int size = 10;
 
 
 	std::cout << "Ingrese 10 Numeros: " << std::endl;
 	
-	for (size_t i = 0; i < 10; i++)
+	for (size_t i = 0; i < size; i++)
 	{
 		std::cout << "Valor#" << i+1 << ": ";
 		std::cin >> inNum[i];
 	}
 	
-	std::cout << "\nEl numero mayor fue: " << numHighest(inNum) << std::endl;
+	std::cout << "\nEl numero mayor fue: " << numHighest(inNum,size) << std::endl;
+	std::cout << "\nEl numero menor fue: " << numLower(inNum,size) << std::endl;
+	std::cout << "\nEl total de la suma de los numero es: " << suma(inNum,size) << std::endl;
+	std::cout << "\nEl promedio es:  " << average(inNum,size) << std::endl;
+
 
 	system("pause");
 
 	
 }
 
-double numHighest(double inNum[])
+double numHighest(double inNum[], int size)
 {
 	double topNum = 0;
-	for (size_t i = 0; i < 10; i++)
+	for (size_t i = 0; i < size; i++)
 	{
 		if (inNum[i] > topNum)
 		{
@@ -45,4 +53,35 @@ double numHighest(double inNum[])
 	}
 
 	return topNum;
+}
+
+double numLower(double inNum[], int size)
+{
+	double lowerNum = 0;
+	for (size_t i = 0; i < size; i++)
+	{
+		lowerNum = inNum[0];
+		if (inNum[i] < lowerNum)
+		{
+			lowerNum = inNum[i];
+		}
+	}
+
+	return lowerNum;
+}
+
+double suma(double inNum[], int size) {
+	double total = 0;
+	for (size_t i = 0; i < size; i++)
+	{
+		total += inNum[i];
+	}
+	return total;
+}
+
+double average(double inNum[], int size)
+{
+	double total = suma(inNum, size);
+	double avarage = total / size;
+	return avarage;
 }
