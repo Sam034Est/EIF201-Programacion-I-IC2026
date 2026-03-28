69d6b95d38eadd9d93f7f355fc722c6f8c994230
Sam034Est <samuel.zamora.quintanilla@est.una.ac.cr>
2026-03-27 20:48:31 -0600

    error ejercicio#4 solucionado(mostraba ambos apellidos).

--- sesion02/Ejercicio#4Proce_nombre_completo/FUNTIONS.cpp
+++ sesion02/Ejercicio#4Proce_nombre_completo/FUNTIONS.cpp
@@ -30,30 +30,29 @@ void showData(std::string name)
 	
 	for (size_t i = 0; i < name.length(); i++)
 	{
-		while (lName < name.length()){
+		if (lName < name.length()) {
 			lName += 1;
 		}
 
 		if (name[lName] == ' ')
 		{
 			lName = i;	
-			
 			break;
 		}
 
-	}
+  	}
 		
-		for (size_t i = 0; i < name.length(); i++)
+	for (size_t i = 0; i < name.length(); i++)
+	{
+		if( name[i] == 'A' || name[i] == 'a' ||
+			name[i] == 'E' || name[i] == 'e' ||
+			name[i] == 'I' || name[i] == 'i' ||
+			name[i] == 'O' || name[i] == 'o' ||
+			name[i] == 'U' || name[i] == 'u')
 		{
-			if( name[i] == 'A' || name[i] == 'a' ||
-				name[i] == 'E' || name[i] == 'e' ||
-				name[i] == 'I' || name[i] == 'i' ||
-				name[i] == 'O' || name[i] == 'o' ||
-				name[i] == 'U' || name[i] == 'u')
-			{
-				nVowels++;
-			}
+			nVowels++;
 		}
+	}
 		 
 		std::cout << "Primer Nombre: " << name.substr(0, fName) << std::endl;
 		std::cout << "Primer Apellido: " << name.substr(fName += 1, lName) << std::endl;
