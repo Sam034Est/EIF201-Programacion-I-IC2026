47d8ab05d43712aa7d7700244edaa2bc12bbb5c0
Sam034Est <samuel.zamora.quintanilla@est.una.ac.cr>
2026-04-26 00:10:42 -0600

    metodos, para flot cpp

--- sesion05/Lab03_SO05_[Samuel ZamoraQ]/flota.cpp
+++ sesion05/Lab03_SO05_[Samuel ZamoraQ]/flota.cpp
@@ -5,7 +5,65 @@
 namespace UNA {
 	//contructor
 	flota::flota(int capacidadInicial) {
-
+		cantidad = 0;
 		capacidad = capacidadInicial;
+
+		vehiculos = new vehiculo* [capacidadInicial];
+		for (size_t i = 0; i < capacidad; i++)
+		{
+			vehiculos[i] = nullptr;
+		}
 	}
+	//destructor
+	flota::~flota() {
+		for (size_t i = 0; i < cantidad; i++) {
+			delete flota::vehiculos[i];
+			vehiculos[i] = nullptr;
+		}
+		delete[] vehiculos;
+		vehiculos = nullptr;
+	}
+	//metodos
+
+	void flota::redimensionar() {
+		int nuevaCapacidad = capacidad * 2;
+		
+		vehiculo** nuevoArreglo = new vehiculo * [nuevaCapacidad];
+		
+		for (size_t i = 0; i < cantidad; i++)
+		{
+			nuevoArreglo[i] = vehiculos[i];
+		}
+
+		//inicializar las posiciones nuevas
+		for (size_t i = 0; i < cantidad; i++)
+		{
+			nuevoArreglo[i] = nullptr;
+		}
+
+		delete[] vehiculos;
+
+		vehiculos = nuevoArreglo;
+
+		capacidad = nuevaCapacidad;
+	}
+
+
+	void flota::agregar(vehiculo* nuevo) {
+
+		if (nuevo == nullptr) {
+			std::cout << "Error";
+		}
+		if (cantidad == capacidad) {
+			redimensionar();
+		}
+		vehiculos[cantidad] = nuevo;
+		cantidad++;
+	}
+
+	std::string* flota::buscarPorPlaca(std::string placa) {
+			
+	}
+
+
 }
--- sesion05/Lab03_SO05_[Samuel ZamoraQ]/flota.h
+++ sesion05/Lab03_SO05_[Samuel ZamoraQ]/flota.h
@@ -5,25 +5,25 @@
 
 
 
-namespace UNA{
+namespace UNA {
 
 	class flota
 	{
 
 	private:
-		UNA::vehiculo** unidades; //arreglo dinámico
-		int cantidad = 0;	
-		int capacidad = 0;
+		UNA::vehiculo** vehiculos; //arreglo dinámico
+		int cantidad;	
+		int capacidad;
+
+		void redimensionar();
 
 	public:
 
 		flota(int capacidadInicial = 3);
-		
 		~flota();
 
-
 		void agregar(vehiculo* nuevo);
-		std::string* buscarPorPlaca(std::string placa);
+		std::string* buscarPorPlaca(std::string placa);	
 		void mostrarPorMarca(std::string marca);
 		bool eliminar(std::string placa);
 		int contarActivos();
